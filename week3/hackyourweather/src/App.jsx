a15fe8cc61f732e96e7c8e378c047569f868497b
Aleksandr Shenshin <shenshin@me.com>
2021-01-30 23:55:48 +0100

    add searchlist

--- week3/hackyourweather/src/App.jsx
+++ week3/hackyourweather/src/App.jsx
@@ -8,7 +8,7 @@ dotenv.config();
 
 function App() {
   const [cityName, setCityName] = useState('');
-  const [cityWeather, setCityWeather] = useState({});
+  const [searchList, setSearchList] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -16,11 +16,15 @@ function App() {
     try {
       setError(null);
       setLoading(true);
-      if (!cityName) throw new Error('You have not specified a city!');
+      /*
+      Only allow a user to use the "Search City" button when the input
+      field has at least 1 character
+      */
+      if (cityName.length < 1) throw new Error('City name should be at least 1 character long!');
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`);
       if (!response.ok) throw new Error(response.statusText);
       const weatherData = await response.json();
-      setCityWeather(weatherData);
+      setSearchList((oldList) => oldList.push(weatherData));
     } catch (err) {
       setError(err.message);
     } finally {
@@ -36,7 +40,9 @@ function App() {
         setCity={setCityName}
         searchCity={searchCity}
       />
-      <City cityWeather={cityWeather} error={error} loading={loading} />
+      {searchList.map((cityWeather) => (
+        <City key={cityWeather.name} cityWeather={cityWeather} error={error} loading={loading} />
+      ))}
     </div>
   );
 }
