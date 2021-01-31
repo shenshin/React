a1cfa8f40d6a0c57b23ba8a1d34564ce11857996
Aleksandr Shenshin <shenshin@me.com>
2021-01-31 02:34:15 +0100

    End up with week3 part of a project

--- week3/hackyourweather/src/App.jsx
+++ week3/hackyourweather/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { v4 as uuid } from 'uuid';
 import dotenv from 'dotenv';
 import './App.css';
 import City from './components/City';
@@ -24,13 +25,22 @@ function App() {
       const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${process.env.REACT_APP_OPENWEATHERMAP_API_KEY}&units=metric`);
       if (!response.ok) throw new Error(response.statusText);
       const weatherData = await response.json();
-      setSearchList((oldList) => oldList.push(weatherData));
+      weatherData.id = uuid();
+      /*
+      Any time a user searches for a new city, add it to a list of already searched cities
+      */
+      setSearchList([weatherData, ...searchList]);
     } catch (err) {
       setError(err.message);
     } finally {
       setLoading(false);
     }
   };
+
+  /*
+  Allow a user to delete a search entry, by clicking the "X"
+  */
+  const removeCard = (id) => setSearchList(searchList.filter((city) => city.id !== id));
 
   return (
     <div className="App">
@@ -40,9 +50,22 @@ function App() {
         setCity={setCityName}
         searchCity={searchCity}
       />
-      {searchList.map((cityWeather) => (
-        <City key={cityWeather.name} cityWeather={cityWeather} error={error} loading={loading} />
-      ))}
+      {error && <p>{error}</p>}
+      {loading && !error && (
+      <p>
+        Loading weather data...
+      </p>
+      )}
+      {searchList.length > 0
+        ? searchList.map((cityWeather) => (
+          <City
+            key={cityWeather.id}
+            id={cityWeather.id}
+            cityWeather={cityWeather}
+            removeCard={removeCard}
+          />
+        ))
+        : !loading && <p>Enter city name</p>}
     </div>
   );
 }
